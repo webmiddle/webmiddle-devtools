f786b1cfb1ab1f67d0d5926c419b748790641089
Manuel Dell'Elce <mh025r0x@gmail.com>
2017-05-20 15:57:07 +0200

    Resources tree: styles

--- app/components/Resources/Resources.js
+++ app/components/Resources/Resources.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import SplitPane from 'react-split-pane';
 import TreeView from 'react-treeview';
 import { Tabs, Tab } from 'react-draggable-tab';
@@ -19,11 +20,18 @@ function makePath(parentPath, node) {
   return parentPath ? `${parentPath}.${node.name}` : node.name;
 }
 
+function isFileSelected(selectedFilePath, parentPath, i) {
+  return selectedFilePath &&
+    selectedFilePath.folderPath === parentPath &&
+    selectedFilePath.index === i;
+}
+
 // A list of folders/files
 class ResourcesTreeView extends Component {
   static propTypes = {
     nodeList: PropTypes.array.isRequired,
     parentPath: PropTypes.string,
+    selectedFilePath: PropTypes.object,
 
     resourcesActions: PropTypes.object.isRequired,
   };
@@ -50,9 +58,12 @@ class ResourcesTreeView extends Component {
             {node.type === 'folder' ?
               <TreeView
                 nodeLabel={
-                  <span>
-                    {iconsByType[node.type]} <span>{node.name}</span>
-                  </span>
+                  <div className={styles.folder} onClick={() => this.handleArrowClick(i)}>
+                    <span className={styles.label}>
+                      <span className={styles.icon}>{iconsByType[node.type]}</span>
+                      <span className={styles.name}>{node.name}</span>
+                    </span>
+                  </div>
                 }
                 collapsed={node.collapsed}
                 onClick={() => this.handleArrowClick(i)}
@@ -61,16 +72,23 @@ class ResourcesTreeView extends Component {
                   <ResourcesTreeView
                     nodeList={node.children}
                     parentPath={makePath(this.props.parentPath, node)}
+                    selectedFilePath={this.props.selectedFilePath}
                     resourcesActions={this.props.resourcesActions}
                   />
                 }
               </TreeView>
             :
               <div
-                className={styles.file}
+                className={classNames(styles.file, {
+                  [styles.selected]: isFileSelected(this.props.selectedFilePath, this.props.parentPath, i)
+                })}
                 onClick={() => this.handleFileClick(i)}
               >
-                {iconsByType[node.type]} <span>{node.name}</span>
+                <div className={styles.background} />
+                <div className={styles.label}>
+                  <span className={styles.icon}>{iconsByType[node.type]}</span>
+                  <span className={styles.name}>{node.name}</span>
+                </div>
               </div>
             }
           </div>
@@ -154,6 +172,7 @@ export default class Resources extends Component {
             <ResourcesTreeView
               nodeList={this.props.nodeList}
               resourcesActions={this.props.resourcesActions}
+              selectedFilePath={this.props.openFilePaths.list[this.props.selectedFileIndex]}
             />
           </div>
           <div className={styles.openFiles}>
