11627ae9147dfc9dd23f54c321db5fb6ac346370
Manuel Dell'Elce <mh025r0x@gmail.com>
2018-04-15 13:49:42 +0200

    VirtualDetails: Fix attributes regression

--- src/components/Timeline/VirtualDetails.js
+++ src/components/Timeline/VirtualDetails.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Inspector from '../Inspector';
 
-import { transformData } from '../../utils/timeline';
+import { transformData, transformDataObj } from '../../utils/timeline';
 
 import styles from './Timeline.module.scss';
 
@@ -14,7 +14,7 @@ const VirtualDetails = ({ node }) => (
     </li>
     <li>
       <span className={styles.detailsLabel}>Attributes: </span>
-      <Inspector data={transformData(node.value.attributes)} />
+      <Inspector data={transformDataObj(node.value.attributes)} />
     </li>
     <li>
       <span className={styles.detailsLabel}>Result: </span>
