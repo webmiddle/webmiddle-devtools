f462fe336ed5b200921cf400afd62efe52beaeab
Manuel Dell'Elce <mh025r0x@gmail.com>
2018-09-29 12:53:10 +0200

    Timeline: fix margins

--- src/components/Timeline/TimelineNodeDetails.js
+++ src/components/Timeline/TimelineNodeDetails.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import Inspector from "../Inspector";
+import cn from "classnames";
 
 import VirtualDetails from "./VirtualDetails";
+import Inspector from "../Inspector";
 
 import styles from "./Timeline.module.scss";
 
@@ -16,9 +17,14 @@ class TimelineNodeDetails extends Component {
   };
 
   render() {
+    const empty = !this.props.node;
     return (
-      <div className={styles.timelineNodeDetails}>
-        {!this.props.node ? (
+      <div
+        className={cn(styles.timelineNodeDetails, {
+          [styles.empty]: empty
+        })}
+      >
+        {empty ? (
           "Click on a node on the left panel to inspect it."
         ) : this.props.node.type === "virtual" ? (
           <VirtualDetails node={this.props.node} />
