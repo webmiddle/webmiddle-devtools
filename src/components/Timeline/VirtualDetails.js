ae67f45b0a49ef3e7f42de782d15d08f33479f65
Manuel Dell'Elce <mh025r0x@gmail.com>
2018-09-29 13:37:42 +0200

    Timeline: update details style

--- src/components/Timeline/VirtualDetails.js
+++ src/components/Timeline/VirtualDetails.js
@@ -8,16 +8,29 @@ import styles from "./Timeline.module.scss";
 
 const VirtualDetails = ({ node }) => {
   const virtual = node.value.value; // { type: 'virtual', value: { type: 'virtual', value: VIRTUAL } }
+  const name = virtual.type.name || virtual.type.value;
+  const attributes = parseDataObj(virtual.attributes);
+  const attributeNames = Object.keys(attributes);
   return (
-    <ul>
-      <li>
-        <span className={styles.detailsLabel}>Type: </span>
-        <Inspector data={parseData(virtual.type)} />
-      </li>
-      <li>
-        <span className={styles.detailsLabel}>Attributes: </span>
-        <Inspector data={parseDataObj(virtual.attributes)} />
-      </li>
+    <ul className={styles.virtualDetails}>
+      <li className={styles.detailsHead}>{`<${name}>`}</li>
+
+      {attributeNames.length > 0 ? (
+        <li>
+          <span className={styles.detailsLabel}>Props: </span>
+          <ul>
+            {attributeNames.map(attrName => (
+              <li key={attrName}>
+                <span>{attrName}: </span>
+                <Inspector data={attributes[attrName]} />
+              </li>
+            ))}
+          </ul>
+        </li>
+      ) : (
+        ""
+      )}
+
       <li>
         <span className={styles.detailsLabel}>Result: </span>
         <Inspector data={parseData(node.result)} />
