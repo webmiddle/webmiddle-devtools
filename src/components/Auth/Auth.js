16362eae01216f39c17d3eb4583b5057c9b7360b
Manuel Dell'Elce <mh025r0x@gmail.com>
2018-10-09 19:30:01 +0200

    Auth: show loading while connecting

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -14,6 +14,7 @@ import { connect } from "react-redux";
 import styles from "./Auth.module.scss";
 import { getStoredData } from "../../actions/auth";
 import * as noty from "../../utils/noty";
+import Loading from "../Loading/Loading";
 
 class Auth extends Component {
   static propTypes = {
@@ -41,6 +42,7 @@ class Auth extends Component {
     const disabled = !hostname.trim() || !port.trim() || server.connecting;
     return (
       <div className={styles.container} data-tid="container">
+        {server.connecting && <Loading />}
         <form onSubmit={this.handleSubmit}>
           <Field
             name="hostname"
